00fda35d1c5d0d4c725ac425680bec5cd1822d08
Trolann <trevor@mathisen.dev>
2023-04-15 21:04:13 -0700

    more logs updates

--- src/parking-api/mariadb.py
+++ src/parking-api/mariadb.py
@@ -6,17 +6,7 @@ from api_log import BotLog
 from time import sleep
 
 logger = BotLog('api-mariadb')
-@dataclass
-class Garage:
-    name: str
-    address: str
-    fullness: int
-    timestamp: str
 
-    def get_tuple(self):
-        return self.name, self.address, self.fullness, self.timestamp
-
-# ... (previous code)
 class Config:
     def __init__(self):
         # Try to connect to the MySQL server without an SSH tunnel
@@ -30,23 +20,10 @@ class Config:
 
         if self.conn:
             logger.info(f'Connected to MariaDB host {getenv("DB_HOST")}')
-            #self.heartbeat()  # Start the heartbeat function
         else:
             logger.error(f'Could not connect to MariaDB host {getenv("DB_HOST")}')
             self.__del__()
 
-    def heartbeat(self):
-        while True:
-            sleep(5)
-            try:
-                self.conn.ping(reconnect=True) # Ping the server to keep the connection alive
-                sleep(60 * 15) # Wait for 60 seconds before pinging again
-                logger.debug(f'Heartbeat sent to MariaDB host {getenv("DB_HOST")}')
-            except mysql.connector.errors.InterfaceError:
-                logger.error(f'Lost connection to MariaDB host {getenv("DB_HOST")}')
-                self.conn.reconnect() # Reconnect to the server
-            except:
-                logger.error(f'Unknown error occurred while pinging MariaDB host {getenv("DB_HOST")}')
     def __del__(self):
         self.conn.close()
 
---
+++ src/prompts.md
@@ -0,0 +1,20 @@
+# What is this file?
+
+These prompts were used but didn't fit elsewhere and we ~~needed~~ wanted to keep track of them 
+
+## Comment prompt
+
+Almost every file was ran through this prompt to generate standardized comments.
+If it wasn't ran through this prompt it was because it was easily commented and we definitely
+did it after we had this prompt and knew we would ask for it later.
+
+### System message:
+
+```Take in the given python code and output the same exact python code with added comments and logger statements.
+Do not over comment but make sure major steps or complex statements are explained.
+Use PEP8 standards and make docstrings for all functions.```
+
+### User message: 
+```Do not give me anything besides python code formatted correctly with the necessary comments.
+Do not explain code, add ``` markdown ticks, or give me anything other than code which can
+be pasted right back into the program with added comments```
